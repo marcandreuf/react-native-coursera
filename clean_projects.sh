5e8788d3c93059eff7e67e25a012a73384952da7
Marc Andreu <marc@itqualab.com>
2024-07-23 12:47:56 +0900

    prep capstone project

--- clean_projects.sh
+++ clean_projects.sh
@@ -2,7 +2,7 @@
 
 # Find and delete all "node_modules" folders
 find . -maxdepth 3 -type d -name "node_modules" -exec rm -rf {} +
-find . -maxdepth 3 -type d -name ".git" -exec rm -rf {} +
+find . -maxdepth 3 -type d -wholename "./**/.git" -exec rm -rf {} +
 find . -maxdepth 3 -type d -name ".expo" -exec rm -rf {} +
 
 echo "All the projects have been cleaned up."
